71bc8e4ebf1a6839271ad0d415fa5b2d2de142e0
Jose Antonio <josemf93@gmail.com>
2022-08-17 19:32:03 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { contadorModule } from './contador/contador/contador.module';
+import { heroesModule } from './heroes/heroes.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    heroesModule,
+    contadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
